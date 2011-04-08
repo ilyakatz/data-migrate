7910f3acaad78d9d962edb6fb152569aa61e0f12
Andrew J Vargo <ajvargo@computer.org>
2011-04-07 21:00:24 -0400

    Data Migration generator

---
+++ lib/generators/data_migration/data_migration_generator.rb
@@ -0,0 +1,28 @@
+require 'rails/generators/migration'
+class DataMigrationGenerator < Rails::Generators::NamedBase
+  source_root File.expand_path('../templates', __FILE__)
+  include Rails::Generators::Migration
+
+  class_option :skip_migration, :desc => 'Dont generate database migration file.', :type => :boolean
+
+  def create_data_migration
+    set_local_assigns!
+    unless  options.skip_migration?
+      migration_template "migration.rb", "db/migrate/#{file_name}.rb"
+    end
+    migration_template "migration.rb", "db/data/#{file_name}.rb"
+  end
+
+  protected
+  attr_reader :migration_action
+
+  def self.next_migration_number(dirname)
+    Time.now.utc.strftime("%Y%m%d%H%M%S")
+  end
+
+  def set_local_assigns!
+    if file_name =~ /^(add|remove)_.*_(?:to|from)_(.*)/
+      @migration_action = $1
+    end
+  end
+end
---
+++ lib/generators/data_migration/templates/migration.rb
@@ -0,0 +1,9 @@
+class <%= migration_class_name %> < ActiveRecord::Migration
+  def up
+
+  end
+
+  def down
+
+  end
+end
