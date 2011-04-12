96896907555b6f9fe122c462771489c65b9a0664
Andrew J Vargo <ajvargo@computer.org>
2011-04-11 22:44:48 -0400

    * Remove Comments   * Tidy code

--- lib/generators/data_migration/install_generator.rb
+++ lib/generators/data_migration/install_generator.rb
@@ -1,29 +1,16 @@
-# require 'rails/generators/migration'
-# class InstallGenerator < Rails::Generators::Base
-#   source_root File.expand_path('../templates', __FILE__)
-#   include Rails::Generators::Migration
-
-#   def data_migrations_table_migration
-#     generate("migration", "add_data_migrations_table version:text")
-#     migration_template "install_migration.rb", "db/migrate/create_data_migrations.rb"
-#   end
-# end
-
-
-#   protected
-#   def self.next_migration_number(dirname)
-#      Time.now.utc.strftime("%Y%m%d%H%M%S")
-#    end
-# end
 require 'rails/generators'
 require 'rails/generators/migration'
+
 module DataMigration
   class InstallGenerator < Rails::Generators::Base
     include Rails::Generators::Migration
-    def self.source_root
-      @source_root ||= File.join(File.dirname(__FILE__), 'templates')
+    source_root File.expand_path('../templates', __FILE__)
+
+    def create_migration_file
+      migration_template "install_migration.rb", "db/migrate/create_data_migrations.rb"
     end
 
+    protected
     def self.next_migration_number(dirname)
       if ActiveRecord::Base.timestamped_migrations
         Time.new.utc.strftime("%Y%m%d%H%M%S")
@@ -31,11 +18,5 @@ module DataMigration
         "%.3d" % (current_migration_number(dirname) + 1)
       end
     end
-
-    def create_migration_file
-      migration_template "install_migration.rb", "db/migrate/create_data_migrations.rb"
-      #migration_template 'migration.rb', 'db/migrate/create_pages_table.rb'
-    end
   end
-
 end
