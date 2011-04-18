51fdb124b36de2d2c77e1fe7e8a214da019b3338
Andrew J Vargo <ajvargo@computer.org>
2011-04-17 23:21:16 -0400

    Remove rails code

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-SkepticalResources::Application.config.secret_token = 'd64c2fdbf904b786abba47051368514069e84c68081606621070d73db95d9fc22c4e091d2e17ec7f6eca5efd3989e123811cb867e9c5375bfd1c3503ba09b39f'
--- config/initializers/session_store.rb
+++
@@ -1,8 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-SkepticalResources::Application.config.session_store :cookie_store, :key => '_skeptical_resources_session'
-
-# Use the database for sessions instead of the cookie-based default,
-# which shouldn't be used to store highly confidential information
-# (create the session table with "rails generate session_migration")
-# SkepticalResources::Application.config.session_store :active_record_store
