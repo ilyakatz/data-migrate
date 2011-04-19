30c9241f894cbdccbbc63562bf176208f1ff6514
Andrew J Vargo <ajvargo@computer.org>
2011-04-18 22:13:23 -0400

    Make index of data migrations table match that of schema migrations.

--- lib/generators/data_migrate/install/templates/install_migration.rb
+++ lib/generators/data_migrate/install/templates/install_migration.rb
@@ -1,10 +1,10 @@
 class CreateDataMigrations < ActiveRecord::Migration
   def self.up
     create_table :data_migrations do |t|
-      t.string :version
+      t.string :version, :null => false
     end
 
-    add_index :data_migrations, :version
+    add_index :data_migrations, :version, :unique => true, :name => "<%= ActiveRecord::Base.table_name_prefix %>unique_data_migrations<%= ActiveRecord::Base.table_name_suffix %>"
   end
 
   def self.down
