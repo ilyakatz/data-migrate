27968ba864005b4355ce4ff0b07061b036fa6f35
Daniel Orner <daniel.orner@wishabi.com>
2014-08-18 14:35:18 -0400

    Fix for classes that inherit from something other than ActiveRecord::Base

--- lib/generators/data_migration/templates/migration_include.rb
+++ lib/generators/data_migration/templates/migration_include.rb
@@ -1,4 +1,4 @@
-<% options.classes.each do |class_name| %>class <%= class_name %> < ActiveRecord::Base
+<% options.classes.each do |class_name| %>class <%= class_name %> < <%= class_name.constantize.superclass.name %>
 <% class_name.constantize.reflect_on_all_associations.each do |assoc| %>  <%= assoc.macro %> :<%= assoc.name %>, <%= assoc.options %>
 <% end %>end
 
