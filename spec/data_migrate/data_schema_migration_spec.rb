2b5f57b631f8415eec6f3fe96b4aa710945395c0
Ilya Katz <ilyakatz@gmail.com>
2016-10-15 14:49:03 -0700

    one more test

--- spec/data_migrate/data_schema_migration_spec.rb
+++ spec/data_migrate/data_schema_migration_spec.rb
@@ -2,7 +2,15 @@ require 'spec_helper'
 
 describe DataMigrate::DataSchemaMigration do
   let(:subject) { DataMigrate::DataSchemaMigration }
-  it "returns correct table name" do
-    expect(subject.table_name).to eq("data_migrations")
+  describe :table_name do
+    it "returns correct table name" do
+      expect(subject.table_name).to eq("data_migrations")
+    end
+  end
+
+  describe :index_name do
+    it "returns correct index name" do
+      expect(subject.index_name).to eq("unique_data_migrations")
+    end
   end
 end
