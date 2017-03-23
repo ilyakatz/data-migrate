4ebd3317478d71aa9482b84e61563bcd9e8c7230
Pierre Michard <pmich35@gmail.com>
2017-03-23 11:17:17 +0100

    use rails migration generator helper

--- lib/generators/data_migration/data_migration_generator.rb
+++ lib/generators/data_migration/data_migration_generator.rb
@@ -1,12 +1,13 @@
 require 'generators/data_migrate'
 require 'rails/generators'
-require 'rails/generators/migration'
+require 'rails/generators/active_record/migration'
 
 module DataMigrate
   module Generators
     class DataMigrationGenerator < Rails::Generators::NamedBase
       namespace "data_migration"
-      include Rails::Generators::Migration
+      include ActiveRecord::Generators::Migration
+
 
       argument :attributes, type: :array, default: [], banner: 'field:type field:type'
 
@@ -16,13 +17,6 @@ module DataMigrate
       end
 
       protected
-
-      attr_reader :migration_action
-
-      def self.next_migration_number(dirname)
-        next_migration_number = current_migration_number(dirname) + 1
-        ActiveRecord::Migration.next_migration_number(next_migration_number)
-      end
 
       def set_local_assigns!
         if file_name =~ /^(add|remove)_.*_(?:to|from)_(.*)/
