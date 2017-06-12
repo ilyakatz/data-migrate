28a67fe26d7f501ef56bb4d795194aa30bcf2443
Joel Turkel <jturkel@salsify.com>
2017-06-12 09:16:32 -0400

    Remove deadcode

--- lib/data_migrate.rb
+++ lib/data_migrate.rb
@@ -1,4 +1,3 @@
 require File.join(File.dirname(__FILE__), 'data_migrate', 'data_migrator')
 require File.join(File.dirname(__FILE__), 'data_migrate', 'data_schema_migration')
-require File.join(File.dirname(__FILE__), 'data_migrate', 'migration')
 require File.join(File.dirname(__FILE__), 'data_migrate', 'railtie')
