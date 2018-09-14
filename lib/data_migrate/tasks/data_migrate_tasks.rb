2ad6b63e8d784c8e2c598dac79c2230af2aea498
Błażej Piaskowski <haradd@users.noreply.github.com>
2018-09-14 10:22:35 +0200

    rename path "data/migrate" to "db/data" to keep consistency

--- lib/data_migrate/tasks/data_migrate_tasks.rb
+++ lib/data_migrate/tasks/data_migrate_tasks.rb
@@ -4,8 +4,10 @@ module DataMigrate
       extend self
       def migrations_paths
         @migrations_paths ||= begin
-          if Rails.application && Rails.application.paths["data/migrate"]
-            Rails.application.paths["data/migrate"].to_a
+          if Rails.application && Rails.application.paths["db/data"]
+            Rails.application.paths["db/data"].to_a
+          else
+            "db/data/"
           end
         end
       end
@@ -16,7 +18,7 @@ module DataMigrate
         if Rails::VERSION::MAJOR == 5 && Rails::VERSION::MINOR == 2
           DataMigrate::MigrationContext.new(migrations_paths).migrate(target_version)
         else
-          paths = migrations_paths || "db/data/"
+          paths = migrations_paths
           DataMigrate::DataMigrator.migrate(paths, ENV["VERSION"] ? ENV["VERSION"].to_i : nil)
         end
       end
