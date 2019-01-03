dbf00e9223bc253285b8ed1865d33e8cc52c04d3
Błażej Piaskowski <haradd@users.noreply.github.com>
2019-01-03 08:56:36 +0100

    remove building absolute path, since `config.data_migrations_path` is a full path

--- lib/data_migrate/status_service.rb
+++ lib/data_migrate/status_service.rb
@@ -11,10 +11,6 @@ module DataMigrate
       @connection = connection
     end
 
-    def root_folder
-      Rails.root
-    end
-
     def dump(stream)
       unless @connection.table_exists?(table_name)
         stream.puts "Data migrations table does not exist yet."
@@ -52,7 +48,7 @@ module DataMigrate
 
     def migration_files(db_list)
       file_list = []
-      Dir.foreach(File.join(root_folder, DataMigrate.config.data_migrations_path)) do |file|
+      Dir.foreach(DataMigrate.config.data_migrations_path) do |file|
         # only files matching "20091231235959_some_name.rb" pattern
         if match_data = DataMigrate::DataMigrator.match(file)
           status = db_list.delete(match_data[1]) ? "up" : "down"
