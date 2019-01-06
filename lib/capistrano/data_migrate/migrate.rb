55de934360ebe089fa80dee44c6de234e52801bb
Tom Reis <tom@commercekitchen.com>
2019-01-06 13:16:55 -0700

    Use double quoted strings

--- lib/capistrano/data_migrate/migrate.rb
+++ lib/capistrano/data_migrate/migrate.rb
@@ -1,33 +1,33 @@
 namespace :deploy do
 
-  desc 'Runs rake data:migrate if migrations are set'
-  Rake::Task['deploy:migrate'].clear_actions
+  desc "Runs rake data:migrate if migrations are set"
+  Rake::Task["deploy:migrate"].clear_actions
   task :migrate => [:set_rails_env] do
     on fetch(:migration_servers) do
       conditionally_migrate = fetch(:conditionally_migrate)
-      info '[deploy:migrate] Checking changes in db directory' if conditionally_migrate
+      info "[deploy:migrate] Checking changes in db directory" if conditionally_migrate
 
       if conditionally_migrate && test(:diff, "-qr #{release_path}/db #{current_path}/db")
-        info '[deploy:migrate] Skip `deploy:migrate` (nothing changed in db directory)'
+        info "[deploy:migrate] Skip `deploy:migrate` (nothing changed in db directory)"
       else
-        info '[deploy:migrate] Run `rake db:migrate:with_data`'
+        info "[deploy:migrate] Run `rake db:migrate:with_data`"
         invoke :'deploy:migrating_with_data'
       end
     end
   end
 
-  desc 'Runs rake db:migrate:with_data'
+  desc "Runs rake db:migrate:with_data"
   task migrating_with_data: [:set_rails_env] do
     on fetch(:migration_servers) do
       within release_path do
         with rails_env: fetch(:rails_env) do
-          execute :rake, 'db:migrate:with_data'
+          execute :rake, "db:migrate:with_data"
         end
       end
     end
   end
 
-  after 'deploy:updated', 'deploy:migrate'
+  after "deploy:updated", "deploy:migrate"
 end
 
 namespace :load do
