95fb64db90325cb05214364c7ccc93aae63a43d1
aandis <abhishekraj10@outlook.com>
2020-09-25 15:57:19 +0530

    Add primary key instead of unique on data_migrations table.
    Closes https://github.com/ilyakatz/data-migrate/issues/151

--- spec/data_migrate/migration.rb
+++ spec/data_migrate/migration.rb
@@ -14,6 +14,6 @@ describe subject do
   it "uses correct index name" do
     expect(subject).to receive(:table_name_prefix) { "" }
     expect(subject).to receive(:table_name_suffix) { "" }
-    expect(subject.index_name).to eq("unique_data_migrations")
+    expect(subject.primary_key).to eq("version")
   end
 end
