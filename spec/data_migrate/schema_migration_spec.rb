61ab8164f5baa1cf7b3ac619f684a7a64aeaf68a
Lindsey Whitley <lindsey.whitley@gusto.com>
2021-03-03 16:18:12 -0800

    Add test for rails 6.1

--- spec/data_migrate/schema_migration_spec.rb
+++ spec/data_migrate/schema_migration_spec.rb
@@ -69,7 +69,7 @@ describe DataMigrate::SchemaMigration do
     end
   end
 
-  if Rails::VERSION::MAJOR == 6 && Rails::VERSION::MINOR == 1
+  if Rails.version >= '6.0.3.5'
     describe :migrations_paths do
       context 'when a db_name is configured' do
         let(:paths) { ["spec/db/migrate/6.0", "spec/db/components/migrate/6.0"] }
