23ba1b7f957480600d0dfe47252342402a1b2450
Tim Aßmann <tassmann@jovoto.com>
2021-12-14 18:13:08 +0100

    undo fix in migration
    it breaks all other rails versions

--- lib/data_migrate/migration.rb
+++ lib/data_migrate/migration.rb
@@ -1,5 +1,5 @@
 module DataMigrate
-  class Migration < ::ActiveRecord::Migration[7.0]
+  class Migration < ::ActiveRecord::Migration
 
     class << self
       def check_pending!(connection = ::ActiveRecord::Base.connection)
