7f3d3f992d2c392ba53ab448fc9c6abea7639298
Guillaume Dott <guillaume+github@dott.fr>
2022-01-18 09:47:54 +0100

    Move status tasks in DataMigrate::Tasks::DataMigrateTasks

--- lib/data_migrate/tasks/data_migrate_tasks.rb
+++ lib/data_migrate/tasks/data_migrate_tasks.rb
@@ -2,6 +2,11 @@ module DataMigrate
   module Tasks
     module DataMigrateTasks
       extend self
+
+      def schema_migrations_path
+        File.join('db', 'migrate')
+      end
+
       def migrations_paths
         @migrations_paths ||= DataMigrate.config.data_migrations_path
       end
@@ -39,6 +44,78 @@ module DataMigrate
         else
           ActiveRecord::Base.dump_schema_after_migration
         end
+      end
+
+      def status
+        config = connect_to_database
+        return unless config
+
+        connection = ActiveRecord::Base.connection
+        puts "\ndatabase: #{config['database']}\n\n"
+        DataMigrate::StatusService.dump(connection)
+      end
+
+      def status_with_schema
+        config = connect_to_database
+        return unless config
+
+        db_list_data = ActiveRecord::Base.connection.select_values(
+          "SELECT version FROM #{DataMigrate::DataSchemaMigration.table_name}"
+        )
+        db_list_schema = ActiveRecord::Base.connection.select_values(
+          "SELECT version FROM #{ActiveRecord::SchemaMigration.schema_migrations_table_name}"
+        )
+        file_list = []
+
+        Dir.foreach(File.join(Rails.root, migrations_paths)) do |file|
+          # only files matching "20091231235959_some_name.rb" pattern
+          if match_data = /(\d{14})_(.+)\.rb/.match(file)
+            status = db_list_data.delete(match_data[1]) ? 'up' : 'down'
+            file_list << [status, match_data[1], match_data[2], 'data']
+          end
+        end
+
+        Dir.foreach(File.join(Rails.root, schema_migrations_path)) do |file|
+          # only files matching "20091231235959_some_name.rb" pattern
+          if match_data = /(\d{14})_(.+)\.rb/.match(file)
+            status = db_list_schema.delete(match_data[1]) ? 'up' : 'down'
+            file_list << [status, match_data[1], match_data[2], 'schema']
+          end
+        end
+
+        file_list.sort!{|a,b| "#{a[1]}_#{a[3] == 'data' ? 1 : 0}" <=> "#{b[1]}_#{b[3] == 'data' ? 1 : 0}" }
+
+        # output
+        puts "\ndatabase: #{config['database']}\n\n"
+        puts "#{"Status".center(8)} #{"Type".center(8)}  #{"Migration ID".ljust(14)} Migration Name"
+        puts "-" * 60
+        file_list.each do |file|
+          puts "#{file[0].center(8)} #{file[3].center(8)} #{file[1].ljust(14)}  #{file[2].humanize}"
+        end
+        db_list_schema.each do |version|
+          puts "#{'up'.center(8)}  #{version.ljust(14)}  *** NO SCHEMA FILE ***"
+        end
+        db_list_data.each do |version|
+          puts "#{'up'.center(8)}  #{version.ljust(14)}  *** NO DATA FILE ***"
+        end
+        puts
+      end
+
+      private
+
+      def connect_to_database
+        config = ActiveRecord::Base.configurations[Rails.env || 'development']
+        ActiveRecord::Base.establish_connection(config)
+
+        unless DataMigrate::DataSchemaMigration.table_exists?
+          puts 'Data migrations table does not exist yet.'
+          config = nil
+        end
+        unless ActiveRecord::SchemaMigration.table_exists?
+          puts 'Schema migrations table does not exist yet.'
+          config = nil
+        end
+        config
       end
     end
   end
