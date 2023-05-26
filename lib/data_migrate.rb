4bbdc2c471f3adf9a1ec7dffa62b3ba125fee68d
Maple Ong <maple.ong@gusto.com>
2023-05-26 10:53:34 -0400

    Correct DataMigrator file name

--- lib/data_migrate.rb
+++ lib/data_migrate.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-require File.join(File.dirname(__FILE__), "data_migrate", "data_migrator_five")
+require File.join(File.dirname(__FILE__), "data_migrate", "data_migrator")
 require File.join(File.dirname(__FILE__), "data_migrate", "data_schema_migration")
 require File.join(File.dirname(__FILE__), "data_migrate", "data_schema")
 require File.join(File.dirname(__FILE__), "data_migrate", "database_tasks")
