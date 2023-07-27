04d7ed7b7c68a1847c22130591e27c8f46a5cb67
Maple Ong <maple.ong@gusto.com>
2023-07-27 15:37:01 -0400

    Remove legacy migrator

--- lib/data_migrate.rb
+++ lib/data_migrate.rb
@@ -10,7 +10,6 @@ require File.join(File.dirname(__FILE__), "data_migrate", "status_service")
 require File.join(File.dirname(__FILE__), "data_migrate", "migration_context")
 require File.join(File.dirname(__FILE__), "data_migrate", "railtie")
 require File.join(File.dirname(__FILE__), "data_migrate", "tasks/data_migrate_tasks")
-require File.join(File.dirname(__FILE__), "data_migrate", "legacy_migrator")
 require File.join(File.dirname(__FILE__), "data_migrate", "config")
 require File.join(File.dirname(__FILE__), "data_migrate", "schema_migration")
 
