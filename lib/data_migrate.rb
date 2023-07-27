f747d5067dc6a27b05a6ef5332ba7185bf14853e
Maple Ong <maple.ong@gusto.com>
2023-07-27 15:36:25 -0400

    Introduce rails helper for varying rails versions

--- lib/data_migrate.rb
+++ lib/data_migrate.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+require File.join(File.dirname(__FILE__), "data_migrate", "rails_helper")
 require File.join(File.dirname(__FILE__), "data_migrate", "data_migrator")
 require File.join(File.dirname(__FILE__), "data_migrate", "data_schema_migration")
 require File.join(File.dirname(__FILE__), "data_migrate", "data_schema")
